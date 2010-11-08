d8b7136475359317043d44859752086a001b2f1c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-08 23:03:45 +0800

    n

--- quick_sort.c
+++ quick_sort.c
@@ -6,19 +6,19 @@ using namespace std;
 
 int main()
 {
-	size=7;
-	Initial();
-	data[0]=0; 
-	data[1]=1;
-	data[2]=2;
- 	data[3]=6;
+  size=7;
+  Initial();
+  data[0]=0; 
+  data[1]=1;
+  data[2]=2;
+  data[3]=6;
   data[4]=4;
-	data[5]=5;
+  data[5]=5;
   data[6]=3;
-	Sort(0,6);
-	Print();	
-	cout<<endl;
-	Finalize();
-	return 0;
+  Sort(0,6);
+  Print();	
+  cout<<endl;
+  Finalize();
+  return 0;
 }
 
--- quick_sort.h
+++ quick_sort.h
@@ -7,57 +7,57 @@ int* data;
 int size;
 void Initial()
 {
-	data= new int[size];
-	fout.open("output.txt");
+  data= new int[size];
+  fout.open("output.txt");
 }
 
 void Print()
 {
-	int i;
-	for(i=0;i<size;i++)
-	{
-		fout<<data[i];
-	}
-	fout<<endl;
+  int i;
+  for(i=0;i<size;i++)
+  {
+    fout<<data[i];
+  }
+  fout<<endl;
 }
 
 
 void Finalize()
 {
-	delete(data);
-	fout.close();
+  delete(data);
+  fout.close();
 }
 
 int Partition(int i,int j)
 {
-	int pivot=data[i];
-	while(i<j)
-	{
-		while(i<j&&data[j]>=pivot)
-			j--;
-		if(i<j)
-			data[i++]=data[j];
-		while(i<j&&data[i]<=pivot)
-			i++;
-		if(i<j)
-			data[j--]=data[i];
-		
-	}
-	data[i]=pivot;
-	return i;
+  int pivot=data[i];
+  while(i<j)
+  {
+    while(i<j&&data[j]>=pivot)
+      j--;
+    if(i<j)
+      data[i++]=data[j];
+    while(i<j&&data[i]<=pivot)
+      i++;
+    if(i<j)
+      data[j--]=data[i];
+
+  }
+  data[i]=pivot;
+  return i;
 }
 
 void Sort(int low,int high)//¹é²¢ÅÅÐò
 {
-	if(low<high)
-	{
-		int pivot;
-		
-		Print();
-		pivot=Partition(low,high);
-		Print();
-		
-		Sort(low,pivot-1);
-		Sort(pivot+1,high);
-	}
+  if(low<high)
+  {
+    int pivot;
+
+    Print();
+    pivot=Partition(low,high);
+    Print();
+
+    Sort(low,pivot-1);
+    Sort(pivot+1,high);
+  }
 }
