026a3271c70ec316390f42d15c99a74cf985768c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-08 23:02:07 +0800

    n

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int main()
 {
-	QuickSort *qs=new QuickSort(7);
+  QuickSort *qs=new QuickSort(7);
 
-	qs->data[0]=0; 
-	qs->data[1]=1;
-	qs->data[2]=2;
- 	qs->data[3]=6;
+  qs->data[0]=0; 
+  qs->data[1]=1;
+  qs->data[2]=2;
+  qs->data[3]=6;
   qs->data[4]=4;
-	qs->data[5]=5;
+  qs->data[5]=5;
   qs->data[6]=3;
-	qs->Sort(0,6);
-	qs->Print();	
-	cout<<endl;
-	
-	return 0;
+  qs->Sort(0,6);
+  qs->Print();	
+  cout<<endl;
+
+  return 0;
 }
 
--- quick_sort.h
+++ quick_sort.h
@@ -5,73 +5,73 @@ using namespace std;
 
 class QuickSort
 {
-private:
-	ofstream fout; 
+  private:
+    ofstream fout; 
 
-public:
-	int* data;
-	int size;
-	QuickSort(int len);
-	~QuickSort();
-	void Print();
-	void Sort(int low,int high);//πÈ≤¢≈≈–Ú
-	int Partition(int i,int j);
+  public:
+    int* data;
+    int size;
+    QuickSort(int len);
+    ~QuickSort();
+    void Print();
+    void Sort(int low,int high);//πÈ≤¢≈≈–Ú
+    int Partition(int i,int j);
 };
 
 QuickSort::QuickSort(int len)
 {
-	size=len;
-	data= new int[size];
-	fout.open("output.txt");
+  size=len;
+  data= new int[size];
+  fout.open("output.txt");
 }
 
 void QuickSort::Print()
 {
-	int i;
-	for(i=0;i<size;i++)
-	{
-		fout<<data[i];
-	}
-	fout<<endl;
+  int i;
+  for(i=0;i<size;i++)
+  {
+    fout<<data[i];
+  }
+  fout<<endl;
 }
 
 
 QuickSort::~QuickSort()
 {
-	delete(data);
-	fout.close();
+  delete(data);
+  fout.close();
 }
 
 int QuickSort::Partition(int i,int j)
 {
-	int pivot=data[i];
-	while(i<j)
-	{
-		while(i<j&&data[j]>=pivot)
-			j--;
-		if(i<j)
-			data[i++]=data[j];
-		while(i<j&&data[i]<=pivot)
-			i++;
-		if(i<j)
-			data[j--]=data[i];
-		
-	}
-	data[i]=pivot;
-	return i;
+  int pivot=data[i];
+  while(i<j)
+  {
+    while(i<j&&data[j]>=pivot)
+      j--;
+    if(i<j)
+      data[i++]=data[j];
+    while(i<j&&data[i]<=pivot)
+      i++;
+    if(i<j)
+      data[j--]=data[i];
+
+  }
+  data[i]=pivot;
+  return i;
 }
 
 void QuickSort::Sort(int low,int high)//πÈ≤¢≈≈–Ú
 {
-	if(low<high)
-	{
-		int pivot;
-		cout<<"1"<<endl;
-		Print();
-		pivot=Partition(low,high);
-		Print();
-		
-		Sort(low,pivot-1);
-		Sort(pivot+1,high);
-	}
+  if(low<high)
+  {
+    int pivot;
+    cout<<"1"<<endl;
+    Print();
+    pivot=Partition(low,high);
+    Print();
+
+    Sort(low,pivot-1);
+    Sort(pivot+1,high);
+  }
 }
