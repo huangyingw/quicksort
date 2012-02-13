dc95638ee9bd75e3e3bee5f33cad43db6016babc
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-13 16:44:51 +0800

    n

--- src/quicksort.java
+++ src/quicksort.java
@@ -1,13 +1,15 @@
 public class quicksort {
+	private static int count = 0;
+
 	static public void main(String[] args) {
 		quicksort qs = new quicksort(7);
-		qs.data[0] = 0;
+		qs.data[0] = 3;
 		qs.data[1] = 1;
 		qs.data[2] = 2;
 		qs.data[3] = 6;
 		qs.data[4] = 4;
 		qs.data[5] = 5;
-		qs.data[6] = 3;
+		qs.data[6] = 0;
 		quicksort.count = 0;
 		qs.Sort(0, 6);
 		System.out.println(qs.toString());
@@ -37,9 +39,7 @@ public class quicksort {
 		System.out.println(qs.toString());
 		System.out.println(quicksort.count);
 	}
-
 	private int[] data;
-	private static int count = 0;
 
 	int size;
 
