063b3e317347d47d6421bc4b94e19ade1b3491e7
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-08 17:13:27 -0500

    n

--- src/test.java
+++ src/test.java
@@ -4,5 +4,35 @@ public class test {
 		quicksort qs = new quicksort(data);
 		qs.Sort();
 		System.out.println(qs.toString());
+
+		data = new int[] { 1 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
+
+		data = new int[] { 1, 2 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
+
+		data = new int[] { 1, 2, 3 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
+
+		data = new int[] { 1 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
+
+		data = new int[] { 2, 1 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
+
+		data = new int[] { 3, 2, 1 };
+		qs = new quicksort(data);
+		qs.Sort();
+		System.out.println(qs.toString());
 	}
 }
