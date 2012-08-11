be086223c510f8de8b629a098386ad22ed662180
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-11 12:05:48 -0500

    n

--- src/Log.java
+++ src/Log.java
@@ -1,10 +1,13 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Log {
 	public static void main(String[] args) throws IOException {
 		Log log = new Log("log.txt");
+		log.Append("I love you");
+		log.Append("I love you");
 		log.Append("I love you");
 	}
 
@@ -15,12 +18,20 @@ public class Log {
 	public Log(String name) throws IOException {
 		super();
 		this.name = name;
+		File file = new File(name);
+		if (file.exists()) {
+			file.delete();
+		}
 		bw = new BufferedWriter(new FileWriter(this.name, true));
 	}
 
+	public void Clear() {
+		// bw.
+	}
+
 	public void Append(String line) throws IOException {
-		line += "\n";
 		bw.write(line, 0, line.length());
+		bw.newLine();
 		bw.flush();
 	}
 }
