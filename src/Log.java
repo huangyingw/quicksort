948b3592e2fea6f475d7a2a6081ee9f125915565
huangyingw@linux_programming <huangyingw@gmail.com>
2012-08-11 11:29:39 -0500

    n

--- src/Log.java
+++ src/Log.java
@@ -1,4 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Log {
-
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter("log.txt", true));
+		String newStr = "新的内容\n";
+		bw.write(newStr, 0, newStr.length());
+		bw.flush();
+		bw.close();
+	}
 }
