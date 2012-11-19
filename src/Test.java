57574b22e2810de1a57bf8bcc3fefc70e5899158
huangyingw@linux_programming <huangyingw@gmail.com>
2012-11-19 00:18:44 -0800

    n

--- src/Test.java
+++ src/Test.java
@@ -10,19 +10,9 @@ public class Test {
 		q3.ThreeSort();
 		System.out.println(q3.toString());
 
-		data = new int[] { 7, 6, 5, 5, 4, 4, 3, 2, 2, 1 };
-		q3 = new quicksort(data);
-		q3.Sort();
-		System.out.println(q3.toString());
-
 		data = new int[] { 3, 1, 7, 0, 6, 2, 5, 4 };
 		q3 = new quicksort(data);
 		q3.ThreeSort();
-		System.out.println(q3.toString());
-
-		data = new int[] { 3, 1, 7, 0, 6, 2, 5, 4 };
-		q3 = new quicksort(data);
-		q3.Sort();
 		System.out.println(q3.toString());
 	}
 }
